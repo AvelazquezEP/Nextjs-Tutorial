3deed2729bfbb1c1a7b26a4d9610b5fb51716d2f
Avelazquez <avelazquez@abogadoericprice.com>
2024-02-19 14:33:06 +0100

    UPDATE: the dashboard page, now we have al the data from the database and does not a hardcode data

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -1,18 +1,18 @@
 import { Inter, Lusitana } from 'next/font/google';
 
-// export const inter = Inter({ subsets: ['latin'] });
-// export const lusitana = Lusitana({
-//     weight: ['400', '700'],
-//     subsets: ['latin'],
-// });
+export const inter = Inter({ subsets: ['latin'] });
+export const lusitana = Lusitana({
+    weight: ['400', '700'],
+    subsets: ['latin'],
+});
 
-export function inter2() {
-    Inter({ subsets: ['latin'] });
-}
+// export function inter2() {
+//     Inter({ subsets: ['latin'] });
+// }
 
-export function lusitana2() {
-    Inter({
-        weight: ['400', '700'],
-        subsets: ['latin']
-    });
-}
+// export function lusitana2() {
+//     Inter({
+//         weight: ['400', '700'],
+//         subsets: ['latin']
+//     });
+// }
